e2db42770266bb903bee3be1788b0c788042ad0c
Alexey <alexm20games@gmail.com>
2023-12-22 22:54:33 +0300

    alert

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@ import Footer from '@/components/app/Footer.vue'
 
 <template>
   <div class="container">
+    <div class="alert">
+      <p class="alert__message">&#x26A0;Attention This website will be migrated soon to new domain. Website will be completely rewritten &#x26A0;注意 このウェブサイトは間もなく新しいドメインに移行されます。ウェブサイトは完全に書き換えられます</p>
+    </div>
     <Header />
     <RouterView v-slot="{ Component, route}">
       <Transition :name="route.meta.transition" mode="out-in">
@@ -223,7 +226,7 @@ import Footer from '@/components/app/Footer.vue'
   position: relative;
   display: flex;
   flex-direction: column;
-  padding-top: 3rem;
+  padding-top: 1rem;
   min-height: 100vh;
   z-index: 1;
 }
@@ -253,4 +256,53 @@ import Footer from '@/components/app/Footer.vue'
   transform: translateX(-50px);
   opacity: 0;
 }
+
+// Alert
+.alert{
+  border: 3px solid black;
+  border-radius: 5px;
+  overflow: hidden;
+  // height: 25px;
+  background: rgba(51, 51, 51, 0.3);
+  padding: 5px;
+  border-radius: 15px;
+  &__message {
+    /* animation properties */
+    -moz-transform: translateX(100%);
+    -webkit-transform: translateX(100%);
+    transform: translateX(100%);
+    
+    -moz-animation: alert-animation 40s linear infinite;
+    -webkit-animation: alert-animation 40s linear infinite;
+    animation: alert-animation 40s linear infinite;
+    width: auto;
+    text-wrap: nowrap;
+  }
+}
+
+/* for Firefox */
+@-moz-keyframes alert-animation {
+  from { -moz-transform: translateX(100%); }
+  to { -moz-transform: translateX(-100%); }
+}
+
+/* for Chrome */
+@-webkit-keyframes alert-animation {
+  from { -webkit-transform: translateX(100%); }
+  to { -webkit-transform: translateX(-100%); }
+}
+
+@keyframes alert-animation {
+  from {
+    -moz-transform: translateX(100%);
+    -webkit-transform: translateX(100%);
+    transform: translateX(100%);
+  }
+  to {
+    -moz-transform: translateX(-100%);
+    -webkit-transform: translateX(-100%);
+    transform: translateX(-200%);
+  }
+}
+
 </style>
